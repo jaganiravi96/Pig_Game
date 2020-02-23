10d8ef05fcdeee3dbfcf92ed5659135d81afd0f8
jaganiravi96 <32170018+jaganiravi96@users.noreply.github.com>
2020-02-23 16:06:17 +0530

    Adding Html, css and images for webpage
    The template is created with HTML, CSS and image file. JAvascript logic is added for random number creation

---
+++ Pig_Game/js/app.js
@@ -0,0 +1,30 @@
+/*
+GAME RULES:
+
+- The game has 2 players, playing in rounds
+- In each turn, a player rolls a dice as many times as he whishes. Each result get added to his ROUND score
+- BUT, if the player rolls a 1, all his ROUND score gets lost. After that, it's the next player's turn
+- The player can choose to 'Hold', which means that his ROUND score gets added to his GLBAL score. After that, it's the next player's turn
+- The first player to reach 100 points on GLOBAL score wins the game
+
+*/
+
+var scores, roundScore, dice, activePlayer, hold;
+
+scores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+hold = 0;
+
+dice = Math.floor(Math.random() * 6 + 1);
+
+document.querySelector('#current-'+ activePlayer).textContent = dice;
+document.querySelector('.dice').style.display = 'none';
+
+
+
+
+
+
+
+
