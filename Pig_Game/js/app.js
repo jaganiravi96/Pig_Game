deabc8059d785002f2f0074604cb3a2ebb17a7b3
Ravi Jagani <jaganiravi96@gmail.com>
2020-02-27 23:12:11 +0530

    adding javascript changes for dice score reflection

--- Pig_Game/js/app.js
+++ Pig_Game/js/app.js
@@ -9,18 +9,54 @@ GAME RULES:
 
 */
 
-var scores, roundScore, dice, activePlayer, hold;
+var scores, roundScore, activePlayer, hold;
 
 scores = [0,0];
 roundScore = 0;
 activePlayer = 0;
 hold = 0;
 
-dice = Math.floor(Math.random() * 6 + 1);
-
-document.querySelector('#current-'+ activePlayer).textContent = dice;
 document.querySelector('.dice').style.display = 'none';
 
+document.getElementById('score-0').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+document.querySelector('.btn-roll').addEventListener('click', function(){
+    
+    //Get Random Number
+    var dice = Math.floor(Math.random() * 6 + 1);
+    
+    //show dice with that number
+    var diceDOM = document.querySelector('.dice');
+    diceDOM.style.display = 'block';
+    diceDOM.src = '../resource/dice-'+dice+'.png';
+    
+    //Update player round score if the rolled number is not 1.
+   if(dice != 1){
+        roundScore = roundScore + dice;
+        parseInt(document.getElementById('current-'+activePlayer).textContent) = roundScore;
+        console.log(document.getElementById('score-'+activePlayer).value);
+    }
+    else{
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+        roundScore = 0;
+    }
+    
+   /* var temp = 0;
+    if(activePlayer == 0){
+         temp = document.getElementById('score-0').;
+         temp = temp + roundScore; 
+         document.getElementById('score-0').textContent = temp;
+        //activePlayer = 1;    
+    }
+    else{
+        document.getElementById('score-1').textContent += roundScore;
+        //activePlayer = 0;
+    }*/
+    
+});
 
 
 
